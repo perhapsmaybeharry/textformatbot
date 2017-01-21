4ab91887b521b71a911acc823f9625c91a5d2d23
perhapsmaybeharry <perhapsmaybeharry@users.noreply.github.com>
2017-01-21 17:14:02 +0800

    Initial Commit

---
+++ format-bot/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  format-bot
+//
+//  Created by Harry Wang on 21/1/17.
+//  Copyright © 2017 thisbetterwork. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
